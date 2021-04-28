aac35fc3566029f235b9c784d99ce2bc4b6cf3e4
CUDDAPAH NIKHIL SAI <61914929+nikhilsai992@users.noreply.github.com>
2021-04-28 15:04:20 +0530

    Delete documentation/html directory

--- documentation/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['embedded_20c_20programming_20examples_20with_20continuous_20integration_20and_20code_20quality_0',['Embedded C Programming Examples with Continuous Integration and Code Quality',['../md_D__embedded_kosam_README.html',1,'']]],
-  ['embeddedproject_20by_20nikhilsai_20_40subpage_20main_2ec_1',['EmbeddedProject by NikhilSai @subpage main.c',['../index.html',1,'']]]
-];
--- documentation/html/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['embedded_20c_20programming_20examples_20with_20continuous_20integration_20and_20code_20quality_7',['Embedded C Programming Examples with Continuous Integration and Code Quality',['../md_D__embedded_kosam_README.html',1,'']]]
-];

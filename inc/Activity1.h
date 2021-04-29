22d4f0aa563d51dfa30945b17c68e1cf920b7cc0
CUDDAPAH NIKHIL SAI <61914929+nikhilsai992@users.noreply.github.com>
2021-04-29 13:53:47 +0530

    Delete inc directory

--- inc/Activity1.h
+++
@@ -1,5 +0,0 @@
-#ifndef __ACTIVITY1_H__
-#define __ACTIVITY1_H__
-void initializeports();
-#define CHECK (!((PIND)&(3<<PD0)))
-#endif
--- inc/Activity2.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ACTIVITY2_H__
-#define __ACTIVITY2_H__
-void initializeADC();
-uint16_t ReadADC(uint8_t ch);
-
-#endif
